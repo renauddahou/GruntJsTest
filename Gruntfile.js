01f6d99c8030ff10b6c650236e64725278896c00
renauddahou <85571576+renauddahou@users.noreply.github.com>
2022-10-29 15:12:53 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,13 @@
-  grunt.loadNpmTasks('grunt-wiredep');
+module.exports = function( grunt ){
 
-  grunt.initConfig({
-    wiredep: {
-      target: {
-        src: 'index.html' // point to your HTML file.
-      }
-    }
-  });
+    grunt.loadNpmTasks('grunt-wiredep');
+
+    grunt.initConfig({
+        wiredep: {
+            target: {
+                src: 'index.html' // point to your HTML file.
+            }
+        }
+    });
+
+};
