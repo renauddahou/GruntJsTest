19ab49f42db708db0c3c6bfb4a990b9ed3e4654a
renauddahou <85571576+renauddahou@users.noreply.github.com>
2022-11-01 15:40:55 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,31 @@
-module.exports = function( grunt ){
-
-    grunt.loadNpmTasks('grunt-wiredep');
-
+module.exports = function(grunt) {
     grunt.initConfig({
-        wiredep: {
-            target: {
-                src: 'index.html' // point to your HTML file.
+        package: grunt.file.readJSON('package.json')
+        uglify: {
+            t1: {
+                files: [
+                    {
+                        src: ['src/a.js', 'src/b.js'],
+                        dest: 'build/ab.min.js'
+
+                    },
+                    {
+                        src: ['src/a.js', 'src/c.js'],
+                        dest: 'build/ac.min.js'
+
+                    }
+                ]
+            },
+            t2: {
+                files: [
+                    {
+                        src: 'src/*.js',
+                        dest: 'build/abc.min.js'
+                    }
+                ]
             }
         }
     });
-
-};
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('compressJS','uglify');
