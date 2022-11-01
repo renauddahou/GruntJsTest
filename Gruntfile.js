954a9b17a5f082b1919c21eb6487fdc49ed81f11
renauddahou <85571576+renauddahou@users.noreply.github.com>
2022-11-01 17:49:05 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,19 @@ module.exports = function(grunt) {
         package: grunt.file.readJSON('package.json'),
         uglify: {
             t1: {
-                files:
-                {
-                    src: ['src/a.js', 'src/b.js'],
-                    dest: 'build/ab.min.js'
-                },
-            },
-            
+                files: [
+                    {
+                        src: ['src/a.js', 'src/b.js'],
+                        dest: 'build/ab.min.js'
+
+                    },
+                    {
+                        src: ['src/a.js', 'src/c.js'],
+                        dest: 'build/ac.min.js'
+
+                    }
+                ]
+            }
         }
     });
     
